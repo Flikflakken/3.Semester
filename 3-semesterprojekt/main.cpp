2817597f9e8a9346133a203536ad453cfe2a2128
Mikkel Weimar <mikkelweimar@gmail.com>
2022-09-14 14:04:44 +0200

    første program op og køre
    Det første programstykke

---
+++ 3-semesterprojekt/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "Det her skulle gerne være noget vi kunne bruge i Github!" << endl;
+    return 0;
+}
