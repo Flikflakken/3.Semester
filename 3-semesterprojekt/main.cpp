f05e2cf1958a26e6313b75dc0dab0b443faa6ba4
Mikkel <mikkelweimar@gmail.com>
2022-09-23 12:26:32 +0200

    Mikkel har tilføjet en funktion til at vise billede fra webcam

--- 3-semesterprojekt/main.cpp
+++ 3-semesterprojekt/main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
-//Tilføj en kommentar!
+//-----Camera funktion---
+//åbner webcam fra din maskine. HVIS det er en VM og Linux, skal der installeres "Cheese"(Dette er webcam app til linux.)
+//og der skal hentes pakker til dit VM software, for at tillade brugen af webcam. VirtualBox: https://www.virtualbox.org/wiki/Downloads
+int camera(){
+    Mat camera;
 
-//en ny test!
-
-//endnu en ændring!
-
-//test122
+    namedWindow("Display window");
+    VideoCapture cap(0);
+    if(!cap.isOpened()){
+        cout << "cannot open camera" << endl;
+    }
+    while (true){
+        cap >> camera;
+        imshow("Display window", camera);
+        char c= (char)
+        waitKey(20);
+             if (c == 27) {
+                    break;
+                }
+    }
+    return 0;
+}
 
 int main()
 {
     cout << "Hello World!" << endl;
-    cout << "Det her skulle gerne være noget vi kunne bruge i Github!" << endl;
+    camera();
     return 0;
 }
